151caecdc3e9184d34c7ff5cd5a36a3f458eef1b
kozhushna <k.kozhushna@gmail.com>
2023-06-04 10:14:15 +0300

    Add services

---
+++ src/services/user-service.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://64664418ba7110b6639c25a1.mockapi.io/api/v1';
+
+const getUsers = async (page, pageSize) => {
+  const response = await axios.get('/users', {
+    params: {
+      page: page,
+      limit: pageSize,
+    },
+  });
+  return {
+    users: response.data,
+    total: 20,
+  };
+};
+
+export default getUsers;
