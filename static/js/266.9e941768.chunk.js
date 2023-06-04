33dfed79f16b78e7a74f107ae021b9bf1821545a
kozhushna <k.kozhushna@gmail.com>
2023-06-04 20:10:34 +0300

    Updates

--- static/js/266.9e941768.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktweets_app=self.webpackChunktweets_app||[]).push([[266],{266:function(e,s,n){n.r(s);var t=n(184);s.default=function(){return(0,t.jsx)("main",{children:(0,t.jsx)("section",{children:(0,t.jsxs)("div",{children:[(0,t.jsx)("h1",{children:"Welcome to Tweets"}),(0,t.jsx)("h2",{children:"Tweets"})]})})})}}}]);
-//# sourceMappingURL=266.9e941768.chunk.js.map
